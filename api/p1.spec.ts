1a707d2dd9c3854b1a5b9123f30ed3782377ab42
Benjamin Gröhbiel <ben@boclips.com>
2019-09-27 09:44:00 +0100

    Add smoke test for users logging in
    [#167063919]
    
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

---
+++ api/p1.spec.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+
+describe("teachers user", () => {
+  it("can log in", (done) => {
+    request('https://api.boclips.com/v1')
+      .post("/token")
+      .send(
+        `grant_type=password&username=${process.env.TEACHERS_USERNAME}&password=${process.env.TEACHERS_PASSWORD}&client_id=teachers`
+      )
+      .expect(200, done)
+  });
+});
