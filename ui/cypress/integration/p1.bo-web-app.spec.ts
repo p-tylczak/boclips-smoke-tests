21833ba94060b7d3a88c01622ff3b469c50c5bca
Rafal Lewandowski <rafal@boclips.com>
2021-03-08 15:57:33 +0100

    Update the app hostname in the test
    As sad as it is, we should be accessing videos, not tazingo ;(
    
    [#176907150]

--- ui/cypress/integration/p1.bo-web-app.spec.ts
+++ ui/cypress/integration/p1.bo-web-app.spec.ts
@@ -1,6 +1,6 @@
 context('Boclips web app', () => {
     it('can visit the landing page', () => {
-        cy.visit('https://tazingo.boclips.com');
+        cy.visit('https://videos.boclips.com');
         cy.get('#username').type(Cypress.env('PUBLISHER_USERNAME'));
         cy.get('#password').type(Cypress.env('PUBLISHER_PASSWORD'));
         cy.get('#kc-form-login').submit();
