48d20059b4b9714fc09ba7f569798f304f417b24
Boisababy Clips <bob@fleming.nowhere>
2021-03-24 23:52:56 +0000

    Demote publisher spec down to p2
    This reverts commit 7d9684219a08aefe8f681f2c1bc5fc368c316b3c.

--- ui/cypress/integration/publisher.spec.ts
+++ ui/cypress/integration/publisher.spec.ts
@@ -1,4 +1,4 @@
-context('Boclips web app', () => {
+context('Publishers', () => {
     it('can visit the landing page', () => {
         cy.visit('https://videos.boclips.com');
         cy.get('#username').type(Cypress.env('PUBLISHER_USERNAME'));
@@ -6,4 +6,4 @@ context('Boclips web app', () => {
         cy.get('#kc-form-login').submit();
         cy.findByRole('button', {name: /Search/i}).should('exist');
     })
-});
+})
