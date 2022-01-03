9f88906d96ee7a249f8ed32011be67258f9b5003
Remigiusz Oleszczuk <remy@boclips.com>
2022-01-03 17:35:38 +0100

    Add reduced index size test to ui
    - when searching for cats in search should return more than 10k videos
      to detect if index size is reduced
    
      [#180229942]

--- ui/cypress/integration/p1.bo-web-app.spec.ts
+++ ui/cypress/integration/p1.bo-web-app.spec.ts
@@ -1,9 +1,24 @@
 context('Boclips web app', () => {
-    it('can visit the landing page', { retries: 3 }, () => {
+    const username = Cypress.env('PUBLISHER_USERNAME');
+    const password = Cypress.env('PUBLISHER_PASSWORD');
+
+    // it('can visit the landing page', {retries: 3}, () => {
+    //     cy.visit('https://videos.boclips.com');
+    //     cy.get('#username').type(username);
+    //     cy.get('#password').type(password);
+    //     cy.get('#kc-form-login').submit();
+    //     cy.findByRole('button', {name: /Search/i}).should('exist');
+    // })
+
+    it('can detect reduced index size on search page', {retries: 3}, () => {
         cy.visit('https://videos.boclips.com');
-        cy.get('#username').type(Cypress.env('PUBLISHER_USERNAME'));
-        cy.get('#password').type(Cypress.env('PUBLISHER_PASSWORD'));
+        cy.get('#username').type(username);
+        cy.get('#password').type(password);
         cy.get('#kc-form-login').submit();
-        cy.findByRole('button', {name: /Search/i}).should('exist');
+        cy.get('[data-qa=search-input]').type("cats");
+        cy.findByRole('button', {name: /Search/i}).click();
+
+
+        cy.get("[data-qa=search-hits]").contains('10000')
     })
 });
