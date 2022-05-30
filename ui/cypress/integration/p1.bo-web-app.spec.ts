eed6c0ae456b1918a344c154ecd5fe3534853706
Harsha Chaudhary <harsha@boclips.com>
2022-05-30 17:44:23 +0530

    Modify videos to app in the url
    [#182048796]

--- ui/cypress/integration/p1.bo-web-app.spec.ts
+++ ui/cypress/integration/p1.bo-web-app.spec.ts
@@ -2,19 +2,13 @@ context('Boclips web app', () => {
     const username = Cypress.env('PUBLISHER_USERNAME');
     const password = Cypress.env('PUBLISHER_PASSWORD');
 
-    // it('can visit the landing page', {retries: 3}, () => {
-    //     cy.visit('https://videos.boclips.com');
-    //     cy.get('#username').type(username);
-    //     cy.get('#password').type(password);
-    //     cy.get('#kc-form-login').submit();
-    //     cy.findByRole('button', {name: /Search/i}).should('exist');
-    // })
-
     it('can detect reduced index size on search page', {retries: 3}, () => {
-        cy.visit('https://videos.boclips.com');
+        cy.visit('https://app.boclips.com');
         cy.get('#username').type(username);
         cy.get('#password').type(password);
         cy.get('#kc-form-login').submit();
+        cy.findByRole('button', {name: /Search/i}).should('exist');
+
         cy.findByPlaceholderText('Search for videos').type("cats" + '{enter}');
 
 
