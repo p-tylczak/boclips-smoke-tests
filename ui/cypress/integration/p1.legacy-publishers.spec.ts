3b973ad2aeaac021e5b62720a4fc8e71d5e54d3d
Alex Glen <alex@boclips.com>
2022-09-22 16:06:18 +0100

    Remove publishers smoke test
    [#182154949]

--- ui/cypress/integration/p1.legacy-publishers.spec.ts
+++
@@ -1,7 +0,0 @@
-context('legacy-publishers', () => {
-  it('loads the login form', () => {
-    cy.visit('https://publishers.boclips.com/')
-      .get('[data-qa="username"]')
-      .should('be.visible');
-  });
-});
